7a796dc9020bd1471c55571a5afaa4a217220b8e
chardon55 <charlesdong_2000@outlook.com>
2024-02-05 23:32:52 +0800

    Add TOTP test cases

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,5 @@
-use minotp::{GenerateOtp, GenerateOtpDefault, Hotp};
+use data_encoding::BASE32;
+use minotp::{GenerateOtp, GenerateOtpDefault, Hotp, Totp, COMMON_INTERVAL};
 use sha1::Sha1;
 use sha2::{Sha256, Sha512};
 use sha3::Sha3_256;
@@ -27,8 +28,8 @@ test_hotp!(hotp_sha1_digit6_default_str, Sha1, b"98u9U9u(bu(*u(*y89b(b8g", 0x45E
 test_hotp!(hotp_sha1_digit8_default, Sha1, b"98u9U9u(bu(*u(*y89b(b8g", 0x45EE, gen_8, 97147126);
 test_hotp!(hotp_sha1_digit8_default_str, Sha1, b"98u9U9u(bu(*u(*y89b(b8g", 0x45EE, gen_8_str, "97147126");
 
-test_hotp!(hotp_sha1_digit8_generic, Sha1, b"98u9u9u(bu(*u(*y89b(b8g", 0x45EE, gen, 97147126, params: [8]);
-test_hotp!(hotp_sha1_digit10_generic, Sha1, b"98u9u9u(bu(*u(*y89b(b8g", 0x45EE, gen, 997147126, params: [10]);
+test_hotp!(hotp_sha1_digit8_generic, Sha1, b"98u9U9u(bu(*u(*y89b(b8g", 0x45EE, gen, 97147126, params: [8]);
+test_hotp!(hotp_sha1_digit10_generic, Sha1, b"98u9U9u(bu(*u(*y89b(b8g", 0x45EE, gen, 997147126, params: [10]);
 
 test_hotp!(hotp_sha1_digit10_str_leading_zeroes, Sha1, b"98u9U9u(bu(*u(*y89b(b8g", 0x45EE, gen_str, "0997147126", params: [10]);
 
@@ -38,4 +39,31 @@ test_hotp!(hotp_sha512_digit8_default, Sha512, b"111111111111111111111111111", 0
 
 test_hotp!(hotp_sha3_256_digit8_default, Sha3_256, b"111111111111111111111111111", 0xAAAA, gen_8, 49753380);
 
+// TOTP
 
+#[test]
+fn totp_sha1_digit6_default() {
+    let secret_base32 = "MNSTS5LFGEZDQOLF";
+    let timestamp = 1707146471;
+
+    // Decode Base32 using `data_encoding` library
+    let secret = BASE32.decode(secret_base32.as_bytes()).unwrap();
+
+    let totp = Totp::<Sha1>::new(&secret, COMMON_INTERVAL, timestamp).unwrap();
+
+    assert_eq!(totp.remaining_sec(), 19);
+    assert_eq!(totp.gen_6(), 437617);
+}
+
+#[test]
+fn totp_sha256_digit8_default() {
+    let secret_base32 = "K5KEMIDXNB4SA6LPOUQGIZLDN5SGKZBAORUGS4Z7";
+    let timestamp = 1707146946;
+
+    let secret = BASE32.decode(secret_base32.as_bytes()).unwrap();
+
+    let totp = Totp::<Sha256>::new(&secret, 75, timestamp).unwrap();
+
+    assert_eq!(totp.remaining_sec(), 54);
+    assert_eq!(totp.gen_8(), 58273556);
+}
